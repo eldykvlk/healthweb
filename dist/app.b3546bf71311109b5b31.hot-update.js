591c43e0a15f35c3a6979b08a5f6fac2c35c3e25
eldykvlk <eldykoplak@gmail.com>
2023-12-09 15:26:14 +0700

    menambahkan manifest

---
+++ dist/app.b3546bf71311109b5b31.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdatestart0"]("app",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("a4d494a323fba7e81825")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYXBwLmIzNTQ2YmY3MTMxMTEwOWI1YjMxLmhvdC11cGRhdGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7VUFBQSIsInNvdXJjZXMiOlsid2VicGFjazovL3N0YXJ0MC93ZWJwYWNrL3J1bnRpbWUvZ2V0RnVsbEhhc2giXSwic291cmNlc0NvbnRlbnQiOlsiX193ZWJwYWNrX3JlcXVpcmVfXy5oID0gKCkgPT4gKFwiYTRkNDk0YTMyM2ZiYTdlODE4MjVcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
